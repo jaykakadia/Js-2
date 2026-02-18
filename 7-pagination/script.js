7ec3a2eb4d7cd0552c77ce117f43405cccb77897
Jay <jaykakadia3@gmail.com>
2026-02-18 19:33:27 +0530

    minor chages

--- 7-pagination/script.js
+++ 7-pagination/script.js
@@ -5,58 +5,56 @@ const next = document.getElementById("next");
 let currentPage = 1;
 let totalPages = 5;
 next.addEventListener("click", () => {
-    if (currentPage<totalPages){
-        currentPage++;
-        showPage(currentPage);
-        checkButtonState();
-        changeactiveinbottom();
-    }
-
+  if (currentPage < totalPages) {
+    currentPage++;
+    showPage(currentPage);
+    checkButtonState();
+    changeactiveinbottom();
+  }
 });
 prev.addEventListener("click", () => {
-    if (currentPage>1){
-        currentPage--;
-        showPage(currentPage);  
-        checkButtonState();
-        changeactiveinbottom();
-    }
+  if (currentPage > 1) {
+    currentPage--;
+    showPage(currentPage);
+    checkButtonState();
+    changeactiveinbottom();
+  }
 });
 function showPage(page) {
-    document.querySelectorAll(".cards div").forEach((page) => {
-        page.classList.remove("active-page");
-    });
-    document.querySelector(`.Page-${page}`).classList.add("active-page");
+  document.querySelectorAll(".cards div").forEach((page) => {
+    page.classList.remove("active-page");
+  });
+  document.querySelector(`.Page-${page}`).classList.add("active-page");
 }
-function checkButtonState(){
-    if (currentPage === 1){
-        prev.disabled = true;
-    }else{
-        prev.disabled = false;
-    }
-    if (currentPage === totalPages){
-        next.disabled = true;
-    }else{
-        next.disabled = false;
-    }
+function checkButtonState() {
+  if (currentPage === 1) {
+    prev.disabled = true;
+  } else {
+    prev.disabled = false;
+  }
+  if (currentPage === totalPages) {
+    next.disabled = true;
+  } else {
+    next.disabled = false;
+  }
 }
-function changeactiveinbottom(){
-    document.querySelectorAll("#pages a").forEach((page) => {
-        page.classList.remove("active");
-    });
-    document.querySelector(`#pages a:nth-child(${currentPage})`).classList.add("active");
-}   
+function changeactiveinbottom() {
+  document.querySelectorAll("#pages a").forEach((page) => {
+    page.classList.remove("active");
+  });
+  document
+    .querySelector(`#pages a:nth-child(${currentPage})`)
+    .classList.add("active");
+}
 
-// for (let i = 0; i < tabHeader.length; i++) {
-//   tabHeader[i].addEventListener("click", () => {
-//     removeActive();
-//     tabContent[i].classList.toggle("active");
-//     tabHeader[i].classList.toggle("header-active");
-//   });
-// }
+const pageLinks = document.querySelectorAll("#pages a");
 
-// function removeActive() {
-//   for (let i = 0; i < tabHeader.length; i++) {
-//     tabContent[i].classList.remove("active");
-//     tabHeader[i].classList.remove("header-active");
-//   }
-// }
+pageLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    currentPage = parseInt(link.textContent);
+    showPage(currentPage);
+    checkButtonState();
+    changeactiveinbottom();
+  });
+});
